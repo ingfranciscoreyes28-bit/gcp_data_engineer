91d58ba764cde1c17552ae6ae2675bf20bdd4ef6
ingfranciscoreyes28-bit <ing.francisco.reyes28@gmail.com>
2026-04-29 09:12:48 -0400

    ANALYSIS PREPARATION2

--- analysis_preparation/orders_weekly.sql
+++ analysis_preparation/orders_weekly.sql
@@ -14,8 +14,6 @@ CREATE OR REPLACE VIEW ecommerce_analysis.orders_weekly AS
 SELECT
   FORMAT_DATE("%G-W%V", 
   DATE(order_date)) AS week,
-  FORMAT_DATE("%G-W%V", DATE 
-
   COUNT(order_id) AS total_orders,
   SUM(price) AS total_revenue
 FROM ecommerce_analysis.orders_raw
